6e9c7d45115f1e060b586b66bbcb881ca3d13824
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-01-19 14:23:55 -0300

    post option added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
+app.use(express.json());
 
 // Iniciando o Banco de Dados
 mongoose.connect(
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -8,4 +8,9 @@ module.exports = {
         
         return res.json(products);
     },
+    async store(req, res){
+        const product = await Product.create(req.body);
+
+        return res.json(product);
+    }
 };
